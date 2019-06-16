4beb250ee3518ae82e74e32957288e6c7805b522
Dylan Wansbrough <dylan.wansbrough@hotmail.com>
2019-06-16 15:54:30 +1200

    Tutorial

---
+++ BloodEdge/Assets/Scripts/tutorial.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+
+Defines behaviour for tutorial prompts that always appear when the player is in
+a certain area.
+
+*/
+public class tutorial : MonoBehaviour {
+
+	public GameObject uiObject;
+	public GameObject trigger;
+
+	void Start () {
+		uiObject.SetActive(false);
+	}
+
+	void OnTriggerStay (Collider other) {
+		// Do nothing if it isn't the player
+		if(other.gameObject.tag != "Player"){
+			return;
+		}
+		// Activate UI prompt
+		uiObject.SetActive(true);
+	}
+
+	void OnTriggerExit(Collider other) {
+		// Do nothing if it isn't the player
+		if(other.gameObject.tag != "Player"){
+			return;
+		}
+		// Deactivate UI prompt
+		uiObject.SetActive(false);
+		trigger.SetActive(false);
+	}
+}
